ca2b4c4262a4b38bd785b913a6d23879f8f78d09
arifirm <arifirmasya080105@gmail.com>
2022-09-02 12:19:06 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+   // cara menghilangkan tombol cari
+   $('#tombol-cari').hide();
+
+   // event ketika keyword ditulis
+   $('#keyword').on('keyup', function() {
+      // memunculkan icon loading
+      $('.loader').show();
+      
+      // ajax menggunakan load
+      //$('#container').load('ajax/mahasiswa.php?keyword=' + $('#keyword').val());
+
+      $.get('ajax/mahasiswa.php?keyword=' + $('#keyword').val(), function(data) {
+         $('#container').html(data);
+         $('.loader').hide();
+      });
+   });
+});
